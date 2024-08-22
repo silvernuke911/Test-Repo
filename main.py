a6166b261ceaaa7e6f1a00acb52155bf0603ba9b
silvernuke911 <vercilbenben5@gmail.com>
2024-08-22 23:59:06 +0800

    whatever the fuck this is

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 from functions import *
 
 mainpath = os.path.join(r'C:\Users\verci\Documents\Python Code\Test-Repo', 'test images\\')
-filename = 'anti-aliasing god un.png'
+filename = 'telescope1.png'
 
 image_path = mainpath+filename
-num_slices = 10
-output_folder = mainpath+'output_slices'
+num_slices = 20
+output_folder = mainpath+'output_slices1'
 
 def main():
     cut_image_horizontally(image_path, num_slices, output_folder)
